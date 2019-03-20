86b88f0b597fc741cd077469e3017c82c34fab4c
kuangyanit <faq366@163.com>
2019-03-21 03:26:10 +0800

    format fan01

--- tragedy/player/fan01.js
+++ tragedy/player/fan01.js
@@ -5,7 +5,7 @@ module.exports = function (total, records) {
     if (total <= 5) return 1;
     const max = Math.floor(_.maxBy(records, 'count').count);
     const min = Math.floor(_.minBy(records, 'count').count);
-    const half = Math.ceil((max - min)/2);
+    const half = Math.ceil((max - min) / 2);
     const avg = _.meanBy(records, 'count');
     return avg + half;
 };
