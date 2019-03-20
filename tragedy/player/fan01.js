f51a51cc2bac8d3444f1d533ad7aa25838e35ec0
kuangyan <faq366@163.com>
2019-03-20 21:14:41 +0800

    fan03

--- tragedy/player/fan01.js
+++ tragedy/player/fan01.js
@@ -5,7 +5,7 @@ module.exports = function (total, records) {
     if (total <= 5) return 1;
     const totalPeople = records.length;
     const avg =  Math.floor(total / (records.length)  * 2 / 5);
-    const aboveAvgPeople = _.countBy(records, r => r <= avg);
-    if (aboveAvgPeople * 2 > totalPeople) return Math.floor(avg);
+    const aboveAvgPeople = _.countBy(records, r => r >= avg);
+    if (aboveAvgPeople * 2 > totalPeople) return Math.floor(avg) - 1;
     return Math.floor(avg) + 1;
 };
