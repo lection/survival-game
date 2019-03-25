c10ae3c4c15ab8b1ed1f538836e4ec4436926035
yujingwen <lection.yu@gmail.com>
2019-03-25 13:47:51 +0800

    增加公地悲剧平均每轮评分

--- tragedy/player/bob.js
+++ tragedy/player/bob.js
@@ -2,7 +2,7 @@
 
 module.exports = function (total, lastRecord) {
     const COUNT = (total/lastRecord.length);
-    if(COUNT > 3) {
+    if(COUNT > 5) {
         return COUNT * 1.8;
     }else {
         return COUNT * 2/3;
