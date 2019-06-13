d453ac33eb79554d79033a98a5b993a1fc8cc8b3
Roy.Yan <roy2an@163.com>
2019-06-13 19:33:44 +0800

    update

--- hundred/player/boyce.js
+++ hundred/player/boyce.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports = function (maxInfo, secondInfo, total, playerMap, index) {
-    let { score: max_score, name: max_name } = maxInfo;
-    let { score: sec_score, name: sec_name } = secondInfo;
+    // let { score: max_score, name: max_name } = maxInfo;
+    // let { score: sec_score, name: sec_name } = secondInfo;
 
-    return max_score >= 101 ? sec_score - 1 : max_score + 1;
+    // return max_score >= 101 ? sec_score - 1 : max_score + 1;
+    return 0
 };
 
